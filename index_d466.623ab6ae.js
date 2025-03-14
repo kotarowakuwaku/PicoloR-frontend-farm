82c00da9a5e0ec65784ed59d050c4ae4aa8c0bb9
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2025-03-14 23:18:21 +0900

    Updates

---
+++ index_d466.623ab6ae.js
@@ -0,0 +1,23 @@
+(function(_){var filename = ((function(){var _documentCurrentScript = typeof document !== "undefined" ? document.currentScript : null;return typeof document === "undefined" ? require("url").pathToFileURL(__filename).href : _documentCurrentScript && _documentCurrentScript.src || new URL("index_d466.js", document.baseURI).href})());for(var r in _){_[r].__farm_resource_pot__=filename;window['eb393d463cd769029553f2b9bfe90d8e'].__farm_module_system__.register(r,_[r])}})({"2c6e69f1":function e(e,r,o,t){e._m(r);var a=o("7ed15cd5");o("7ea85d02");var c=document.querySelector("#root");a.createRoot(c).render(<Main/>);},"64d75239":function f(f,e,n,o){f._m(e),e.default="/PicoloR-frontend-farm/react.35ef61ed-f49548.svg";},"7ea85d02":function t(t,o,e,d){t._m(o),t.o(o,"Main",()=>s);var c=e("493f9ad4"),n=e("a0fc9dfd"),r=t.i(e("64d75239")),i=t.i(e("a21aec30"));function s(){var o=c._(n.useState(0),2),e=o[0],d=o[1];return<>
+      <div>
+        <a href="https://farmfe.org/"target="_blank">
+          <img src={t.f(i)}className="logo"alt="Farm logo"/>
+        </a>
+        <a href="https://react.dev"target="_blank">
+          <img src={t.f(r)}className="logo react"alt="React logo"/>
+        </a>
+      </div>
+      <h1>Farm + React</h1>
+      <div className="card">
+        <button onClick={()=>d(t=>t+1)}>
+          count is {e}
+        </button>
+        <p>
+          Edit <code>src/main.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Farm and React logos to learn more
+      </p>
+    </>;}},"a21aec30":function o(o,e,f,n){o._m(e),e.default="/PicoloR-frontend-farm/logo.e052fe17-7e9d40.png";},});
+//# sourceMappingURL=index_d466.623ab6ae.js.map
