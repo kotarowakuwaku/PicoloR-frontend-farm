38c6fdbc96568b27583c298fa711dc5b716ac450
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2025-03-15 10:52:51 +0900

    github pagesにデプロイ時に白紙になるエラー修正

--- farm.config.ts
+++ farm.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig } from '@farmfe/core';
 
 export default defineConfig({
-  plugins: ['@farmfe/plugin-react']
+  plugins: ['@farmfe/plugin-react'],
+  compilation: {
+    output: {
+      publicPath: "/PicoloR-frontend-farm/", // GitHub Pages 用にリポジトリ名を設定
+    },
+  },
 });
