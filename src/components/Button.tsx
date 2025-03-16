12aa1944418ba2a899888095570493700adc921b
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2025-03-16 17:25:35 +0900

    buttonコンポーネント修正

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import { Button as AntButton, ConfigProvider } from 'antd';
 import { CSSProperties } from 'react';
+import { ButtonMode } from "../types/ButtonMode";
 
 type ButtonProps = {
     text: string;
-    type: 'green' | 'white';
+    type: ButtonMode;
+    onClick?: () => void;
 };
 
-const Button = ({ text, type }: ButtonProps) => {
+const Button = ({ text, type, onClick }: ButtonProps) => {
     const greenStyle: CSSProperties = {
         width: '100%',
+        height: '100%',
+        fontSize: '1.5rem',
         textAlign: 'center',
         backgroundColor: '#0AC74F',
         color: 'white',
         padding: '17px 0',
         border: '2px solid black',
+        borderRadius: '24px',
     };
 
-    const whiteStyle: CSSProperties = {
+    const dashedStyle: CSSProperties = {
         width: '100%',
+        height: '100%',
+        fontSize: '1.5rem',
         textAlign: 'center',
         padding: '17px 0',
         borderStyle: 'dashed solid',
+        borderRadius: '24px',   
     };
 
     return (
@@ -39,7 +47,7 @@ const Button = ({ text, type }: ButtonProps) => {
                 },
             }}
         >
-            <AntButton style={type === 'green' ? greenStyle : whiteStyle}>
+            <AntButton style={type === ButtonMode.GREEN ? greenStyle : dashedStyle} onClick={onClick}>
                 {text}
             </AntButton>
         </ConfigProvider>
