5aeb6cd50bab092d061f6577022cc8edff306689
Taichi <takezohuto1123@icloud.com>
2025-03-16 17:52:46 +0900

    Modalの白い箱を無効化

--- src/components/CountdownModal.tsx
+++ src/components/CountdownModal.tsx
@@ -1,17 +1,45 @@
 import React from "react";
-import { Modal } from "antd";
+import { ConfigProvider, Modal } from "antd";
+import { css } from "../../styled-system/css";
 
 interface CountdownModalProps {
   visible: boolean;
   count: number;
 }
 
-const CountdownModal: React.FC<CountdownModalProps> = ({ visible, count }) => (
-  <Modal open={visible} title="カウントダウン" footer={null}>
-    <div style={{ textAlign: "center", fontSize: "24px" }}>
-      {count > 0 ? count : ""}
-    </div>
-  </Modal>
-);
+const CountdownModal: React.FC<CountdownModalProps> = ({ visible, count }) => {
+  return (
+    <>
+      <ConfigProvider
+        theme={{
+          components: {
+            Modal: {
+              contentBg: "transparent",
+              boxShadow: "none",
+            },
+          },
+        }}
+      >
+        <Modal
+          open={true}
+          footer={null}
+          closable={false}
+          title={null}
+          className={"custom-modal-content"}
+          background-color={"transparent"}
+        >
+          <div
+            className={css({
+              textAlign: "center",
+              fontSize: "240px",
+            })}
+          >
+            {count > 0 ? count : ""}hoge
+          </div>
+        </Modal>
+      </ConfigProvider>
+    </>
+  );
+};
 
 export default CountdownModal;
