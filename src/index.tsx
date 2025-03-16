cbf0a262e0c9b6fee349cc6d74dc629af0b62ba9
TaichiTakeuchi <145305677+valen0306@users.noreply.github.com>
2025-03-16 19:21:53 +0900

    カウントダウンモーダルの作成 (#11)
    * カウントダウンロジックの追加
    
    * コンポーネント内のロジックを分離
    
    * 非推奨になったModalのパラメータを削除
    
    * Modalの白い箱を無効化
    
    * カウントダウンコンポーネントのデザインを変更
    
    * こんふり修正

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
-import { createRoot } from "react-dom/client";
 import { Main } from "./main";
-import "./index.css";
+import { createRoot } from "react-dom/client";
 import "../styled-system/styles.css";
 
 const container = document.querySelector("#root") as Element;
