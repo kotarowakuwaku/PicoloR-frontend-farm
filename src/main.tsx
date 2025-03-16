d2735dd72c87ac3ebc26947cd5542582177e7aff
YukaChoco <sugar.ore.yk@gmail.com>
2025-03-16 13:11:49 +0900

    ボタンの作成

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,88 @@
-import Header from "./components/Header";
-import { HeaderMode } from "./types/HeaderMode";
+import { Button as AntButton, ConfigProvider } from "antd";
+import { css } from "../styled-system/css";
+import { createStyles } from "antd-style";
+
+const useStyle = createStyles(() => ({
+  homeButton: {
+    textAlign: "center",
+    backgroundColor: "var(--light)",
+    fontSize: "3rem",
+    color: "var(--secondary)",
+    padding: "10px 120px",
+    border: "4px dashed #2d2d2d",
+    borderRadius: "10px",
+    height: "auto",
+    transition: "background-color 0.2s",
+    "&:hover": {
+      backgroundColor: "var(--primary)",
+    },
+  },
+}));
 
 export function Main() {
-  return <Header mode={HeaderMode.GRAY} />;
+  const { styles } = useStyle();
+
+  return (
+    <main
+      className={css({
+        h: "100dvh",
+        w: "100dvw",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "40px",
+      })}
+    >
+      <div
+        className={css({
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        })}
+      >
+        <h2
+          className={css({
+            fontFamily: "Londrina Solid",
+            fontSize: "2.6rem",
+            fontWeight: "bold",
+          })}
+        >
+          色を探せ！ひらめきカラースナップ対決！
+        </h2>
+        <h1>
+          <img
+            className={css({
+              h: "30dvh",
+              transition: "transform 0.2s",
+              "&:hover": {
+                transform: "scale(1.2)",
+              },
+            })}
+            src="/Logo.svg"
+            alt="PicoloRロゴ"
+          />
+        </h1>
+      </div>
+      <ConfigProvider
+        theme={{
+          components: {
+            Button: {
+              defaultHoverBg: "#dddddd",
+              defaultHoverColor: "var(--secondary)",
+              defaultHoverBorderColor: "var(--secondary)",
+            },
+          },
+        }}
+      >
+        <AntButton
+          className={styles.homeButton}
+          onClick={() => console.log("START")}
+        >
+          START
+        </AntButton>
+      </ConfigProvider>
+    </main>
+  );
 }
