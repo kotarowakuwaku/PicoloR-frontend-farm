71c4dadf75c9aa854666878046298abea33e8a60
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2025-03-17 01:54:14 +0900

    ルーティング追加

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Main } from "./main";
 import { Rooms } from "./room";  // 新しく作成したTestコンポーネント
+import { Result } from "./result";
 import "./index.css";
 import "../styled-system/styles.css";
 
@@ -9,10 +10,11 @@ const container = document.querySelector("#root") as Element;
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="PicoloR-frontend-farm/" element={<Main />} />
-      <Route path="PicoloR-frontend-farm/room/:roomId" element={<Rooms />} />  {/* 新しいページ */}
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+        <Routes>
+            <Route path="PicoloR-frontend-farm/" element={<Main />} />
+            <Route path="PicoloR-frontend-farm/room/:roomId" element={<Rooms />} />  {/* 新しいページ */}
+            <Route path="PicoloR-frontend-farm/result/:roomId" element={<Result />} />
+        </Routes>
+    </BrowserRouter>
 );
