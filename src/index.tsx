e5cb5b08fdba044adf6600570353e4f0fd6a93ae
YukaChoco <sugar.ore.yk@gmail.com>
2025-03-17 02:03:05 +0900

    routerにPicoloR-frontend-farm/controller/joinを追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Main } from "./main";
-import { Rooms } from "./room";  // 新しく作成したTestコンポーネント
+import { Rooms } from "./room"; // 新しく作成したTestコンポーネント
 import "./index.css";
 import "../styled-system/styles.css";
+import { ControllerJoin } from "./controllerJoin";
 
 const container = document.querySelector("#root") as Element;
 const root = createRoot(container);
@@ -12,7 +13,11 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="PicoloR-frontend-farm/" element={<Main />} />
-      <Route path="PicoloR-frontend-farm/room/:roomId" element={<Rooms />} />  {/* 新しいページ */}
+      <Route path="PicoloR-frontend-farm/room/:roomId" element={<Rooms />} />
+      <Route
+        path="PicoloR-frontend-farm/controller/join"
+        element={<ControllerJoin />}
+      />
     </Routes>
   </BrowserRouter>
 );
