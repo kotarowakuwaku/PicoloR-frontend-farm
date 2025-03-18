1543cc6997d7bb3fe76066925f5229343ad498d2
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2025-03-19 08:13:02 +0900

    ルーティング変更

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Main } from "./main";
-import { Rooms } from "./room"; // 新しく作成したTestコンポーネント
-import { GameHost } from "./gamehost"; // 新しく作成したTestコンポーネント
+import { Rooms } from "./room";
+import { GameHost } from "./gamehost";
 import { Result } from "./result";
 import { Test } from "./test";
 import "./index.css";
@@ -14,26 +14,15 @@ const container = document.querySelector("#root") as Element;
 const root = createRoot(container);
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/PicoloR-frontend-farm"> {/* ✅ basename を追加 */}
     <Routes>
-      <Route path="PicoloR-frontend-farm/" element={<Main />} />
-      <Route path="PicoloR-frontend-farm/test" element={<Test />} />
-
-
-      <Route path="PicoloR-frontend-farm/room/:roomId" element={<Rooms />} />
-      <Route
-        path="PicoloR-frontend-farm/controller/join"
-        element={<ControllerJoin />}
-
-      />
-      <Route
-        path="PicoloR-frontend-farm/controller"
-        element={<ControllerPlaying />}
-      />
-      <Route path="PicoloR-frontend-farm/result/:roomId" element={<Result />} />
-      <Route
-        path="PicoloR-frontend-farm/room/:roomId/hostUser"
-        element={<GameHost />}/>
+      <Route path="/" element={<Main />} />
+      <Route path="/test" element={<Test />} />
+      <Route path="/room/:roomId" element={<Rooms />} />
+      <Route path="/controller/join" element={<ControllerJoin />} />
+      <Route path="/controller" element={<ControllerPlaying />} />
+      <Route path="/result/:roomId" element={<Result />} />
+      <Route path="/room/:roomId/hostUser" element={<GameHost />} />
     </Routes>
   </BrowserRouter>
 );
