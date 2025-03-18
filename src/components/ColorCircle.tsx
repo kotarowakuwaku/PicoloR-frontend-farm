354eccdac059fd3e2b8a47c17da5a38582ce6ff8
Taichi <takezohuto1123@icloud.com>
2025-03-18 23:40:45 +0900

    とりあえず

--- src/components/ColorCircle.tsx
+++ src/components/ColorCircle.tsx
@@ -7,10 +7,11 @@ interface ColorCircleProps {
   delay: number;
   imageURL?: string;
   rank?: number;
+  name?: string;
 
   onAnimationComplete?: () => void;
 }
-const TestImage = "/test.png";
+// const TestImage = "/test.png";
 
 export const ColorCircle: React.FC<ColorCircleProps> = ({
   color,
@@ -20,6 +21,7 @@ export const ColorCircle: React.FC<ColorCircleProps> = ({
   onAnimationComplete,
 }) => {
   console.log("rank", rank);
+  console.log("passedthemeColor", color);
   const rankImage = () => {
     switch (rank) {
       case 1:
@@ -32,25 +34,25 @@ export const ColorCircle: React.FC<ColorCircleProps> = ({
         return "";
     }
   };
-  imageURL = TestImage;
+  // imageURL = TestImage;
   return (
     <div style={{ position: "relative", width: "20vw", height: "20vw" }}>
-      <img
-        src={rankImage()}
-        alt=""
-        className={css({
-          w: "50%",
-          h: "40%",
-          position: "absolute",
-          transform: "rotate(40deg)",
-          float: "right",
-          top: "-15%",
-          right: "-15%",
-          zIndex: 1,
-        })}
-      />
       {imageURL ? (
         <div style={{ position: "relative", width: "20vw", height: "20vw" }}>
+          <img
+            src={rankImage()}
+            alt=""
+            className={css({
+              w: "50%",
+              h: "40%",
+              position: "absolute",
+              transform: "rotate(40deg)",
+              float: "right",
+              top: "-15%",
+              right: "-15%",
+              zIndex: 1,
+            })}
+          />
           <div
             style={{
               position: "absolute",
@@ -64,7 +66,7 @@ export const ColorCircle: React.FC<ColorCircleProps> = ({
             }}
           ></div>
           <img
-            src="/test.png"
+            src={imageURL}
             alt="Test Image"
             style={{
               position: "absolute",
--- src/gamehost.tsx
+++ src/gamehost.tsx
@@ -9,35 +9,38 @@ import Stopwatch from "./components/Stopwatch";
 import useCountdown from "./hooks/useCountdown";
 import useStopwatch from "./hooks/useStopwatch";
 import { supabase } from "./supabase/supabase";
+import { data } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export function GameHost() {
   const [modalVisible, setModalVisible] = useState(false);
   const [stopwatchVisible, setStopwatchVisible] = useState(false);
+  const { roomId } = useParams<{ roomId: string }>();
+  const [themeColors, setThemeColors] = useState<ColorObj[]>([]);
+  const [imageURL, setImageURL] = useState<string>();
+  const [payloadNew, setPayloadNew] = useState<PayloadNew[]>([]);
+
   const { count, isCounting, startCountdown } = useCountdown(() => {
     setModalVisible(false);
     setStopwatchVisible(true);
     stopwatch.start();
   });
 
-  const stopwatch = useStopwatch();
+  type ColorObj = {
+    ColorId: number;
+    ColorCode: string;
+  };
 
-  //postedtimeとusernameがほしい
-  const mockResponse: any[] = [
-    {
-      themeColor: "#19ff00",
-      imageURL: "aaa.png",
-      rank: 2,
-      postedtime: 45,
-      userid: 15,
-    },
-    // { themeColor: "#004cff", imageURL: "bbb.png", rank: 3 },
-    // { themeColor: "#ff007f", imageURL: "bbb.png", rank: 1 },
-  ];
-  const mockColor = [
-    { themeColor: "#19ff00" },
-    { themeColor: "#ff007f" },
-    { themeColor: "#004cff" },
-  ];
+  type PayloadNew = {
+    color_id: string;
+    image: string;
+    posted_time: string;
+    rank: number;
+    room_id: number;
+    user_id: number;
+  };
+
+  const stopwatch = useStopwatch();
 
   useEffect(() => {
     if (modalVisible) {
@@ -46,27 +49,68 @@ export function GameHost() {
   }, [modalVisible, startCountdown]);
 
   useEffect(() => {
+    const fetchThemeColors = async () => {
+      try {
+        const response = await fetch(
+          `https://picolor-backend-go.onrender.com/controller/colors?roomID=${roomId}`,
+          {
+            method: "GET",
+          }
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const resThemeColor = await response.json();
+        setThemeColors(resThemeColor.themeColors);
+      } catch (error) {
+        console.error("Error fetching theme colors:", error);
+      }
+    };
+    fetchThemeColors();
+  }, [roomId]);
+
+  useEffect(() => {
+    console.log("roomId", roomId);
     const channel = supabase
-      .channel("table_member_db_changes")
+      .channel("table_db_changes")
       .on(
         "postgres_changes",
         {
-          event: "INSERT",
+          event: "*",
           schema: "public",
           table: "posts",
         },
-        (payload) => {
+
+        async (payload) => {
+          console.log("start channel");
+          console.log("payload", payload);
           if (payload.eventType === "INSERT") {
             console.log(payload);
+            console.log(payload.new?.room_id);
+            setPayloadNew((prev) => [payload.new as PayloadNew, ...prev]);
+            console.log("payloadNew", payloadNew);
+
+            if (payload.new?.room_id === Number(roomId)) {
+              const { data: Data, error } = await supabase
+                .from("users")
+                .select("name")
+                .eq("id", payload.new.user_id)
+                .single();
+              if (error) {
+                console.error(error);
+              } else if (Data) {
+                console.log(Data);
+              }
+            }
           }
         }
       )
       .subscribe();
+
     return () => {
       channel.unsubscribe();
     };
   }, []);
-
   return (
     <div
       className={css({
@@ -83,8 +127,23 @@ export function GameHost() {
           padding: "0 15vw",
         })}
       >
-        {mockColor.map(({ themeColor }, index) => (
+        {themeColors.map((colorObj, index) => (
           <ColorCircle
+            key={index}
+            color={colorObj.ColorCode}
+            delay={index * 1.0}
+            imageURL={
+              payloadNew?.find((item) => item.color_id === colorObj.ColorCode)
+                ?.image || ""
+            }
+            // rank={payloadNew?.rank}
+            onAnimationComplete={
+              index === 2 ? () => setModalVisible(true) : undefined
+            }
+          />
+        ))}
+
+        {/* <ColorCircle
             key={index}
             color={themeColor}
             delay={index * 1.0}
@@ -98,9 +157,8 @@ export function GameHost() {
             }
             onAnimationComplete={
               index === 2 ? () => setModalVisible(true) : undefined
-            }
-          />
-        ))}
+            } 
+          />*/}
       </div>
       {modalVisible && <CountDownModal visible={true} count={count} />}
       <div
